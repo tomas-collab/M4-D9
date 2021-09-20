4bdcf12d8d4882d7dd4189d345f3db426271efb9
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-09-20 23:01:38 +0200

    tweak

--- book-shop/src/App.js
+++ book-shop/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Signup } from "./component/SignUp.js";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Registration from "./component/Registration";
 
 const App = () => {
   return(
@@ -10,6 +10,9 @@ const App = () => {
      <div className='row'>
        <div className='col-md-5'>
         <Signup/>
+       </div>
+       <div>
+         <Registration/>
        </div>
        <div className='col-md-7'>
        </div>
--- book-shop/src/component/Registration.jsx
+++ book-shop/src/component/Registration.jsx
@@ -1,32 +1,39 @@
 import { useState } from "react"
-
-const registration=()=>{
+import {Form,Button} from 'react-bootstrap'
+const Registration=()=>{
 
     const [registration, setRegistration] = useState({name:'',surname:'',email:'',password:'',confirmPassword:''})
     const [shopComplete, setShopComplete] = useState(false)
+
+ const handleInput = (key,value)=>{
+          setRegistration(
+             { ...registration,
+            [key]:value}
+            )
+ }
     return(
          <>
          <Form>
 
                 <Form.Group className="mb-3">
                      <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter name" value={registration.name}/>
+                    <Form.Control type="text" placeholder="Enter name" onChange={(e)=>handleInput("name",e.target.value)} value={registration.name}/>
                 </Form.Group>
                 <Form.Group className="mb-3">
                      <Form.Label>Surname</Form.Label>
-                    <Form.Control type="text" placeholder="Enter surname" value={registration.surname}/>
+                    <Form.Control type="text" placeholder="Enter surname" onChange={(e)=>handleInput('surname',e.target.value)}  value={registration.surname}/>
                 </Form.Group>
                 <Form.Group className="mb-3" >
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email"  value={registration.email}/>
+                    <Form.Control type="email" placeholder="Enter email" onChange={(e)=>handleInput('email',e.target.value)}  value={registration.email}/>
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"  value={registration.password}/>
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>handleInput('password',e.target.value)}  value={registration.password}/>
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Confirm password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"  value={registration.confirmPassword}/>
+                    <Form.Control type="password" placeholder="Password" onChange={(e)=>handleInput('confirmPassword',e.target.value)}   value={registration.confirmPassword}/>
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
@@ -35,3 +42,5 @@ const registration=()=>{
          </>
     )
 }
+
+export default Registration
